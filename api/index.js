537f219678ef0f32d7874892068955fe8f85ab98
nishantthakurjune <nishantthakurjune@gmail.com>
2024-10-21 20:05:34 +0530

    create user model

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ mongoose.connect(process.env.MONGO).then(()=>{
 
 const app = express();
 
-app.listen(3000, ()=>{
-    console.log("server is running on port 3000!");
+app.listen(3001, ()=>{
+    console.log("server is running on port 3001");
     
 })
